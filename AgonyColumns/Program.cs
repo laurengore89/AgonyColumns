5433038de05488446d9361a75790638c0c5ce4f8
AppledoreProductions <appledore.productions@gmail.com>
2017-10-30 22:12:59 +0000

    get it usable as a true console app

--- AgonyColumns/Program.cs
+++ AgonyColumns/Program.cs
@@ -7,23 +7,30 @@
     {
         static void Main(string[] args)
         {
-            var consumKey = ConfigurationManager.AppSettings["consumKey"];
+			var status = "";
+
+			if (args.Length > 0)
+			{
+				status = args[0];
+			}
+			else
+			{
+				status = "This is some text which exceeds 140 characters. This is used for testing to prevent actually sending the tweet, while confirming that the OAuth solution is working and all is as it should be.";
+				//status = "Ignore this, still testing.";
+			}
+
+			var consumKey = ConfigurationManager.AppSettings["consumKey"];
             var consumSecret = ConfigurationManager.AppSettings["consumSecret"];
 
             var userToken = ConfigurationManager.AppSettings["userToken"];
             var userSecret = ConfigurationManager.AppSettings["userSecret"];
 
-			var status = "This is some text which exceeds 140 characters. This is used for testing to prevent actually sending the tweet, while confirming that the OAuth solution is working and all is as it should be.";
-			//var status = "Ignore this, still testing.";
-			
 			Console.WriteLine("Sending tweet:");
 			Console.WriteLine(status);
 
 			Console.WriteLine();
 
 			Console.WriteLine(TweetSender.SendTweet(consumKey, consumSecret, userToken, userSecret, status));
-            
-            Console.ReadLine();
         }
     }
 }
